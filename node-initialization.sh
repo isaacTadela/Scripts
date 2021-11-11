dac7cc3ce071134398ca0652d28e94977cd8f170
isaacTadela <47701407+isaacTadela@users.noreply.github.com>
2021-11-12 01:33:43 +0200

    Update node-initialization.sh

--- node-initialization.sh
+++ node-initialization.sh
@@ -20,7 +20,40 @@ $(git --version)
 $(chef -v)' > installed
 
 # Download the Chef cookbook
-git clone https://github.com/isaacTadela/Chef.git
+cd $HOME && git clone https://github.com/isaacTadela/Chef.git
+
+# create the configuration file for consul-template 
+# to render the role with the override_attribute
+echo "vault {
+# This is the address of the Vault leader.
+address      = \"http://$MASTER_IP:8200\"
+# This value can also be specified via the environment variable VAULT_TOKEN.
+token        = \"root\"
+unwrap_token = false 
+renew_token  = false
+}
+ 
+consul {
+ address = \"$MASTER_IP:8500\"
+
+ auth {
+   enabled = true
+   username = \"test\"
+   password = \"test\"
+ }
+}
+ 
+log_level = \"warn\"
+
+# render the role with the new value and re run chef-solo 
+template {
+ source = \"$HOME/Chef/script/consul-mysql-npm-role.tpl\"
+ destination = \"$HOME/Chef/roles/consul-mysql-npm.json\"
+  exec {
+    command = \"sudo chef-solo -c $HOME/Chef/solo.rb -j $HOME/Chef/runlist.json > $HOME/consul-template.log \"
+  }
+}" > $HOME/Chef/script/consul-configuration.hcl
+ 
 
 # Run the cookbook
 sudo chef-solo -c $HOME/Chef/solo.rb -j $HOME/Chef/runlist.json --chef-license accept
