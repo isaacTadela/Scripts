52906d65711ecd1d475b3b0680a158d56fbe0404
isaacTadela <47701407+isaacTadela@users.noreply.github.com>
2021-11-13 12:54:10 +0200

    Update node-initialization.sh

--- node-initialization.sh
+++ node-initialization.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# System 
+# System
 sudo apt update < "/dev/null"
 sudo apt -y upgrade < "/dev/null"
 sudo apt -y install unzip < "/dev/null"
@@ -14,7 +14,7 @@ wget https://packages.chef.io/files/stable/chefdk/4.9.7/ubuntu/20.04/chefdk_4.9.
 sudo dpkg -i chefdk_4.9.7-1_amd64.deb
 rm chefdk_4.9.7-1_amd64.deb
 
-# Check all installed: 
+# Check all installed:
 echo -en '### installed ###\n
 $(git --version)
 $(chef -v)' > installed
@@ -22,17 +22,17 @@ $(chef -v)' > installed
 # Download the Chef cookbook
 cd $HOME && git clone https://github.com/isaacTadela/Chef.git
 
-# create the configuration file for consul-template 
+# create the configuration file for consul-template
 # to render the role with the override_attribute
 echo "vault {
 # This is the address of the Vault leader.
 address      = \"http://$MASTER_IP:8200\"
 # This value can also be specified via the environment variable VAULT_TOKEN.
 token        = \"root\"
-unwrap_token = false 
+unwrap_token = false
 renew_token  = false
 }
- 
+
 consul {
  address = \"$MASTER_IP:8500\"
 
@@ -42,10 +42,10 @@ consul {
    password = \"test\"
  }
 }
- 
+
 log_level = \"warn\"
 
-# render the role with the new value and re run chef-solo 
+# render the role with the new value and re run chef-solo
 template {
  source = \"$HOME/Chef/script/consul-mysql-npm-role.tpl\"
  destination = \"$HOME/Chef/roles/consul-mysql-npm.json\"
@@ -53,7 +53,8 @@ template {
     command = \"sudo chef-solo -c $HOME/Chef/solo.rb -j $HOME/Chef/runlist.json > $HOME/consul-template.log \"
   }
 }" > $HOME/Chef/script/consul-configuration.hcl
- 
+
 
 # Run the cookbook
-sudo chef-solo -c $HOME/Chef/solo.rb -j $HOME/Chef/runlist.json --chef-license accept
+echo 'run: sudo chef-solo -c $HOME/Chef/solo.rb -j $HOME/Chef/runlist.json --chef-license accept'
+# echo 'run: sudo chef-solo -c $HOME/Chef/solo.rb -j $HOME/Chef/runlist.json --chef-license accept'
